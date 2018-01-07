0b748c1bd8b353c8258760e21bec5ce7adbeb049
DEATHWING\Jaap <ajwpelt@gmail.com>
2018-01-07 14:48:53 +0100

    Add test functions

--- js/snake.js
+++ js/snake.js
@@ -327,3 +327,127 @@ function createFoods() {
         }
     }
 }
+
+
+/***************************************************************************
+ **                 Testfuncties                                          **
+ ***************************************************************************/
+
+/**
+    @function testSetup() -> boolean
+    @desc test 1: test de initiele setup.
+                  verwachte uitkomst: 
+                    snake van lengte 2
+                    food
+    @return {boolean} test voldoet aan verwachting
+*/
+function testSetup() {
+    result = true;
+    
+    //setup
+    init();
+    
+    //execute scenario
+    
+    //verify
+    result = result && verifySnake();
+    result = result && verifyFood();
+    
+    return "Test Setup: " + result.toString();
+}
+
+/**
+    @function testSetup() -> boolean
+    @desc test 2: test beweging over een leeg veld.
+                  verwachte uitkomst: 
+                    snake van lengte 2
+    @return {boolean} test voldoet aan verwachting
+*/
+function testBounds() {
+    result = true;
+    
+    //setup
+    init();
+    foods = [];
+    
+    //execute scenario
+    for (var i = 0; i < (WIDTH/R + 3); i++) {
+        move(LEFT);
+    }
+                    
+    //verify
+    result = result && verifySnake();
+    result = result && verifyFood();
+    
+    return "Test Setup: " + result.toString();
+}
+
+/**
+    @function verifySnake() -> boolean
+    @desc verifeer of de snake valide is
+    @return {boolean} snake is valide
+*/
+function verifySnake() {
+    result = true;
+    
+    //verify snake type
+    if (typeof snake !== "object") {
+        result = false;
+        console.log("invalid snake: " + typeof snake);
+    }
+    
+    //verify length
+    if (snake.segments.length < 2) {
+        result = false;
+        console.log("invalid snake length");
+    }
+    
+    //verify head existence
+    if (typeof snake.head !== "object") {
+        result = false;
+        console.log("invalid snake head");
+    }
+    
+    //verify head color
+    if (snake.segments[snake.segments.length-1].color != HEAD) {
+        result = false;
+        console.log("invalid snake head color");
+    }
+    
+    //verify body color
+    for (var i = 0; i < snake.segments.length -1; ++i) {
+        if (snake.segments[i].color != SNAKE) {
+            result = false;
+            console.log("invalid snake body color");
+        }
+    }
+    
+    //verify radius
+    for (var i = 0; i < snake.segments.length; ++i) {
+        if (snake.segments[i].radius != R) {
+            result = false;
+            console.log("invalid snake segment radius");
+        }
+    }
+    
+    //ToDo: Verify if snake is connected
+    //ToDo: Verify is snake is within bounds
+    //ToDo: Verify is there are no collisions.
+    
+    return result;
+}
+
+function verifyFood() {
+    result = true;
+
+    for (var i = 0; i < foods.length; ++i) {
+        if (foods[i].color != FOOD) {
+            result = false;
+        }
+    }
+    
+    //ToDo: Verify is food is within bounds
+    //ToDo: Verify is there are no collisions.
+    
+    return result;
+}
